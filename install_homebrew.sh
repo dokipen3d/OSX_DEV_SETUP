0179745e17cbf644ade33a88d2163a7de8417a9b
Daniel Elliott <danielelliott3d@gmail.com>
2016-02-25 21:15:43 +1300

    Update install_homebrew.sh

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -13,3 +13,25 @@ brew install sdl2
 //to get access to additional homebrew taps do this....
 //https://github.com/Homebrew/homebrew-science
 brew tap homebrew/homebrew-science
+
+cairo
+cmake
+coreutils
+eigen
+emscripten
+freetype: most recent version 2.6.3 not installed
+gcc: most recent version 5.3.0 not installed
+gedit: most recent version 3.18.3 not installed
+glew: most recent version 1.13.0 not installed
+glib: most recent version 2.46.2 not installed
+gmp: most recent version 6.1.0 not installed
+Warning: Skipping gtk+: most recent version 2.24.29 not installed
+Warning: Skipping gtk+3: most recent version 3.18.8 not installed
+Warning: Skipping jemalloc: most recent version 4.0.0 not installed
+Warning: Skipping pyqt: most recent version 4.11.4 not installed
+Warning: Skipping pyside: most recent version 1.2.2_1 not installed
+Warning: Skipping python: most recent version 2.7.11 not installed
+Warning: Skipping qt: most recent version 4.8.7_2 not installed
+Warning: Skipping sdl2: most recent version 2.0.4 not installed
+Warning: Skipping sip: most recent version 4.17 not installed
+Warning: Skipping tbb: most recent version 4.4-20151115 not installed
